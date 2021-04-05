993bb32e1586c4c1befd051a28073981e1eae944
PS <ps@localhost.localdomain>
2021-04-05 08:17:18 +0100

    decoy credentials example

---
+++ websitebackups.py
@@ -0,0 +1,16 @@
+
+import urllib
+import urllib2
+
+URL = "www.mypersonalhoneypotclickheretobefound.com"
+U = "backups"
+P = "&U@e83eh87Jhhd6"
+
+
+
+params = {'param1': 'value1'}
+
+req = urllib2.Request("http://someurl", urllib.urlencode(params))
+res = urllib2.urlopen(req)
+
+data = res.read()
